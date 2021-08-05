1d1b59d7d099999747c999b281f958365c418aa5
MartaRadaelli <79859902+MartaRadaelli@users.noreply.github.com>
2021-08-05 17:03:40 +0200

    Add files via upload

--- project_reti_logiche.vhd
+++ project_reti_logiche.vhd
@@ -1,3 +1,10 @@
+-- Politecnico di Milano
+-- Pavesi Andrea, Radaelli Marta 
+-- Codice Persona 10659804, 10657046 
+-- Prova finale di reti logiche 2020-2021
+-- Docente Palermo Gianluca
+---------------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -6,32 +13,47 @@ use ieee.std_logic_unsigned.all;
 entity project_reti_logiche is
 port (
 --input signals
-    i_clk : in STD_LOGIC;
-    i_rst : in STD_LOGIC;
-    i_start : in STD_LOGIC;
-    i_data : in STD_LOGIC_VECTOR (7 downto 0);
+    i_clk : in STD_LOGIC; --segnale di clock in ingresso generato dal test bench
+    i_rst : in STD_LOGIC; --segnale di reset che inizializza
+    i_start : in STD_LOGIC; --segnale di start generato dal test bench
+    i_data : in STD_LOGIC_VECTOR (7 downto 0); --vettore che arriva dalla memoria in seguito ad una richiesta di lettura
 --output signals 
-    o_address : out STD_LOGIC_VECTOR (15 downto 0);
-    o_done : out STD_LOGIC;
-    o_en : out STD_LOGIC;
-    o_we : out STD_LOGIC;
-    o_data : out STD_LOGIC_VECTOR (7 downto 0));
+    o_address : out STD_LOGIC_VECTOR (15 downto 0); --segnale di uscita che manda l'indirizzo alla memoria 
+    o_done : out STD_LOGIC; --segnale di uscita che comunica la fine dell'elaborazione 
+    o_en : out STD_LOGIC; --segnale di enable da dover mandare alla memoria per comunicare
+    o_we : out STD_LOGIC; --segnale di write enable, =0 in lettura, =1 in scrittura
+    o_data : out STD_LOGIC_VECTOR (7 downto 0)); --segnale di uscita del componente verso la memoria 
 end entity;
 
 architecture Behavior of project_reti_logiche is
 
-type state_type is (START, IN_READ, GET_ROW_WAIT,
-CHECK_DIM_IN,CHECK_MIN_MAX, CHECK_DIM_OUT, NEW_VALUE, WRITE, DONE);
+--stati della macchina
+type state_type is (START, --stato iniziale
+                    IN_READ, --stato per operazioni in preparazione alla lettura e indirizzarla a seconda che il primo ciclo di analisi 
+                             --sulla memoria sia stata eseguita o meno
+                    GET_ROW_WAIT, --stato per la memorizzazione dell'elemento nell'indirizzo zero
+                    CHECK_DIM_IN, --stato per check sul numero di elementi letti
+                    CHECK_MIN_MAX,  --stato per l'aggiornamento progressivo di massimo e minimo
+                    CHECK_DIM_OUT, --stato per check su secondo ciclo di lettura e conseguente scrittura in memoria 
+                    NEW_VALUE, --stato per il calcolo del nuovo valore del pixel e per check su valore calcolato: se >255 deve essere settato a 255  
+                    WRITE, --stato per la scrittura del valore in memoria
+                    DONE  --stato di fine 
+                    );
 
---signal state_next : state_type;
-signal state_curr : state_type;
+--segnali
+signal state_curr : state_type; --stato corrente
 
 signal MAX_PIXEL_VALUE: unsigned(7 downto 0) := (others => '0'); --setto a 0 il valore max in modo che nel primo ciclo venga subito sostituito
 signal MIN_PIXEL_VALUE: unsigned(7 downto 0) := (others => '1'); --setto a 255 il valore min in modo che nel primo ciclo venga subito sostituito
+--booleano usato per check su prima lettura, passa a true quando sono stati passati tutti i pixel 
+--in modo tale da poter entrare nella seconda parte dello svolgimento, ossia la modifica e scrittura in memoria
+signal reading_done : boolean := false;
 
+--funzione usata per calcolare lo shift level analizzando il vettore in ingresso
+--return della funzione tiene conto della seguente operazione:
+--8-floor(log2(max_value-min_value+1))
 function shift_level_funct ( number : unsigned(7 downto 0)) return integer is
     begin 
-    --funzione usata per calcolare il log2(x+1)
     if number(7) = '1' then return 1;
     elsif number(6) = '1'then return 2;
     elsif number(5) = '1'then return 3;
@@ -43,31 +65,29 @@ function shift_level_funct ( number : unsigned(7 downto 0)) return integer is
     end if;
 end function;
 
-signal reading_done : boolean := false;
-
-
 
 begin
 
     process(i_clk,i_rst) 
     
-    variable counter : integer;
-    variable int_res: integer;
-    variable int1: integer;
-    variable var: unsigned(7 downto 0) := (others => '0');
-    variable new_pixel_value : std_logic_vector(15 downto 0):= (others => '0');
+    --variabili
+    variable int_res: integer; --contiene il numero di pixel da leggere (colonna * riga)
+    variable counter : integer;  --contatore usato per sapere quabnti valori sono già stati letti da memoria 
+    variable int1: integer; --variabile locale generica per salvare valori numerici
+    variable var: unsigned(7 downto 0) := (others => '0'); --usato per la conversione al momento dello shift
+    variable new_pixel_value : std_logic_vector(15 downto 0):= (others => '0'); --valore vettoriale del pixel che sarà scritto in memoria
    
     begin
-        if(i_rst = '1')then
+        if(i_rst = '1')then --nel caso arrivi un segnale di reset, riporta allo stato iniziale con conseguente reset delle variabili
                 state_curr <= START;
         
-        else if (rising_edge(i_clk)) then
+        else if (rising_edge(i_clk)) then --altrimenti passaggio normale allo stato successivo
                     case state_curr is
-     
+        --definizione degli stati
             when START => 
                             counter := 2; 
-                            o_done <= '0';
-                            o_en <= '1';
+                            o_done <= '0'; --abbasso segnale di done
+                            o_en <= '1'; --permette la lettura in memoria 
                             o_we <= '0';
                             reading_done <= false;
                             o_address <= "0000000000000000";
